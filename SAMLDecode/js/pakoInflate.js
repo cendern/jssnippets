8e6611ce2b496874902355c00c688780c21d2c17
cendern <Charles.Noble@dmv.ca.gov>
2021-05-05 21:32:31 -0700

    0.0.2

--- SAMLDecode/js/pakoInflate.js
+++ SAMLDecode/js/pakoInflate.js
@@ -1,31 +1,46 @@
 function inflate(str) {
-    var junk = pako.inflateRaw(str);
-    return new TextDecoder("utf-8").decode(junk);
+  var junk = pako.inflateRaw(str);
+  return new TextDecoder("utf-8").decode(junk);
 }
 function decodeButton(inputString, output64, output64inflated) {
-    var b64d = atob(decodeURIComponent(input));
-    if (outputb64) outputb64.value = b64d;
-    if (outputb64ddeflated) outputb64ddeflated.value = inflate(b64d);
+  var b64d = atob(decodeURIComponent(inputString));
+  if (outputb64) outputb64.value = b64d;
+  if (outputb64deflated) outputb64ddeflated.value = inflate(b64d);
 }
 function decodeButtonHelp() {
-    decodeButton(
-        document.getElementById('input').value,
-        document.getElementById('outputb64'),
-        document.getElementById('outputb64ddeflated')
-    )
+  decodeButton(
+    document.getElementById("input").value,
+    document.getElementById("outputb64"),
+    document.getElementById("outputb64deflated")
+  );
 }
 function toggleClass(el, className) {
-    if (el.classList.includes('className')) { el.classList.remove('className') }
+  if (el.classList.includes("className")) {
+    el.classList.remove("className");
+  }
 }
-function toggleHide(el) { toggleClass(el, 'hide'); }
+function toggleHide(el) {
+  toggleClass(el, "hide");
+}
 
-function addEventListenerEach(e) {
-    //find adjacent <a> and <textarea> elements and toggle their 'hide' class
-    const elms = [e, e.nextElementSibling()];
-    if (e.classList.includes('hider')) {
-        elms.push(e.nextElementSibling().nextElementSibling());
-    } else {
-        elms.push(e.previousElementSibling());
-    }
-    elms.forEach(toggleHide);
-};
+function addShowHideEventListener(e) {
+  //find adjacent <a> and <textarea> elements and toggle their 'hide' class
+  const elms = [e, e.nextElementSibling()];
+  if (e.classList.includes("hider")) {
+    elms.push(e.nextElementSibling().nextElementSibling());
+  } else {
+    elms.push(e.previousElementSibling());
+  }
+  elms.forEach(toggleHide);
+}
+
+function addShowHideEventListenerEach(el) {
+  el.addEventListener("click", addShowHideEventListener);
+}
+
+function runOnLoad() {
+  document.querySelector("#btn").addEventListener("click", decodeButtonHelp);
+  document
+    .querySelectorAll("a.hider, a.shower")
+    .forEach(addShowHideEventListenerEach);
+}
